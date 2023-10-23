2361d7cc2ba7eec40202d269039a9525fd78dd40
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-23 16:44:20 -0300

    Initial commit

---
+++ src/test/java/br/com/mfarias25/main/PrimeiroProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mfarias25.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9a615ca3f3a5f38be5fe393479866a3199271dde
Mateus Farias <mateusfariasdev@outlook.com>
2023-11-07 15:29:31 -0300

    Component Scan + Path Params

---
+++ src/main/java/br/com/mfarias25/controller/controller/PrimeiraController.java
@@ -0,0 +1,27 @@
+package br.com.mfarias25.controller.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController //Definição de apiRest
+@RequestMapping("/primeiraController") //Definição de recurso
+public class PrimeiraController {
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parametro é: " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O parametro com metodoComQueryParams é: " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String>  allParams) {
+        return "O parametro com metodoComQueryParams2 é: " + allParams.entrySet();
+    }
+
+
+}
--- src/main/java/br/com/mfarias25/main/PrimeiroProjetoSpringbootApplication.java
+++ src/main/java/br/com/mfarias25/main/PrimeiroProjetoSpringbootApplication.java
@@ -2,8 +2,11 @@ package br.com.mfarias25.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.mfarias25")
 public class PrimeiroProjetoSpringbootApplication {
 
     public static void main(String[] args) {
